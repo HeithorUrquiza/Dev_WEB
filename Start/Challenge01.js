6e6785cd54d88e3ac861d94119be58821ed0aa38
Heithor Urquzia <111160912+HeithorUrquiza@users.noreply.github.com>
2022-12-15 18:53:39 -0300

    att

---
+++ Start/Challenge01.js
@@ -0,0 +1,17 @@
+{
+    console.log("Números ímpares")
+    console.log(1) // valor literal
+    console.log(3)
+    console.log(5)
+    console.log(7)
+    console.log(9)
+}
+
+{
+    console.log("\nNúmeros pares")
+    console.log(2)
+    console.log(4)
+    console.log(6)
+    console.log(8)
+    console.log(10)
+}
