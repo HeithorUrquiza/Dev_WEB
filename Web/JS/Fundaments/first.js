ae83c3421ba2ec217b77f5ff0dbb440edd3b74f4
Heithor Urquzia <111160912+HeithorUrquiza@users.noreply.github.com>
2022-12-16 18:23:00 -0300

    att

---
+++ Web/JS/Fundaments/first.js
@@ -0,0 +1,6 @@
+var breakeCurse = "Hello Word !!"
+let num = 1 + 3
+//const nome
+
+console.log(breakeCurse)
+console.log("1 + 3 é igual a", num)
--- Web_Fundaments/JS/first.js
+++
@@ -1,9 +0,0 @@
-// Comentário para uma linha
-/*
-    Comentário para várias linhas (PS: assim como em JAVA)
-*/ 
-var nome = "caneta"
-let idade = 18
-
-console.log("Hello word!!")
-console.log("1 + 3 é igual a", 1 + 3)
